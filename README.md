968b7d8e23f98f8e2b669db2e8705516ba48ec6f
Piotr Dudek <piotr72@gmail.com>
2020-09-06 21:07:35 -0400

    added CSS styling, working save/get schedule items from local storage

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# dailyPlanner2020
+# Daily Planner 2020 :calendar:
+
+Wherever you are heading to this year, you may enjoy this simple & handy work day scheduler...
+<br>
+### GitHub Page with Live Version: https://piotr72us.github.io/dailyPlanner2020/index.html
+<br>
+### Programming Languages Used:
+
++ JavaScript
+<br>
+    (including `$jQuery` and `moments.js`)
++ CSS
++ HTML
+<br>
+### About this application
++ When you open this page, you will see current date and time displayed in the header.
++ Below, there is a work day scheduler with time slots for standard business hours (9AM - 5PM) and input fields to help you keep track of your daily tasks.
++ Click the "save" icon to save your tasks and to access them later.
++ Time blocks change color based on the current time:
+<br>
+gray = past hour/s
+<br>
+red = current hour
+<br>
+green = hour/s ahead
+<br>
+
+Rest assure knowing that you will not miss an important event any more!
+<br>
+
+### Quick glance at the Daily Planner 2020:
+
+![screenshot](./img/screen.png)
--- logic.js
+++ logic.js
@@ -1,58 +1,79 @@
 // JS
+
 // add current day and date with moments to #currentDay
 var date = moment().format('llll');
+document.getElementById("currentDay").append(date);
+//current hour (24h time) as numeric value
 var currentHour = moment().format('H');
 
-document.getElementById("currentDay").append(date);
-document.getElementById("testing").append(currentHour);
+// variables for text input area of each timeblock
+var text9 = document.querySelector("#text9");
+var text10 = document.querySelector("#text10");
+var text11 = document.querySelector("#text11");
+var text12 = document.querySelector("#text12");
+var text13 = document.querySelector("#text13");
+var text14 = document.querySelector("#text14");
+var text15 = document.querySelector("#text15");
+var text16 = document.querySelector("#text16");
+var text17 = document.querySelector("#text17");
 
-
-
-// JS functions
+// array containing IDs of all timeblocks
 var hourElements = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"];
-var task = $("<textarea>");
-
 
     function readFromLocalStorage () {
-//     <!-- this is to look for anything saved previously in local storage -->
-//         use for loop
+// <!-- this is to look for anything saved previously in local storage -->
+var lastUser = JSON.parse(localStorage.getItem("userInput"));
+
+// assign as text back in element (using its id (specific to each hour)
+text9.textContent = lastUser.text9;
+text10.textContent = lastUser.text10;
+text11.textContent = lastUser.text11;
+text12.textContent = lastUser.text12;
+text13.textContent = lastUser.text13;
+text14.textContent = lastUser.text14;
+text15.textContent = lastUser.text15;
+text16.textContent = lastUser.text16;
+text17.textContent = lastUser.text17;
+
+// for loop
 for (var i = 0; i < hourElements.length; i++) {
 //- read value from time property
-// console.log(hourElements[i]);
-
+//- check current hour vs time slot (to determine color of text area: gray, red or green)
 if (parseInt(currentHour) < parseInt(hourElements[i].match(/(\d+)/))) {
-    // console.log(hourElements[i]);
-    // document.getElementById("hourElements[i].innerHTML").className = "future";
-    // $(hourElements[i].text).addClass("future");
-    // $(hourElements[i].text).removeClass("present");
+    //- assign proper class (.past .present .future) based on comparison to current hour (<, > or =)
     $("#" + hourElements[i]).addClass("future");
-    // $("#" + hourElements[i]).removeClass("present");
-}
-else if (parseInt(currentHour) > parseInt(hourElements[i].match(/(\d+)/))) {
+} else if (parseInt(currentHour) > parseInt(hourElements[i].match(/(\d+)/))) {
     $("#" + hourElements[i]).addClass("past");
+} else {
+    $("#" + hourElements[i]).addClass("present");
+}
+    }
 }
 
-else {$("#" + hourElements[i]).addClass("present")}
+    function writeToLocalStorage () {
+// <!-- this is to save a value from the text area to local storage, using the hour as the key when save button is clicked -->
 
-//- assign as text back in element (using its class (.description) and id (specific to each hour)
+// read text from text area related to schedule hour and trim text         
+var userInput = {
+    text9: text9.value.trim(),
+    text10: text10.value.trim(),
+    text11: text11.value.trim(),
+    text12: text12.value.trim(),
+    text13: text13.value.trim(),
+    text14: text14.value.trim(),
+    text15: text15.value.trim(),
+    text16: text16.value.trim(),
+    text17: text17.value.trim()
+};
 
-//- check current hour vs time slot (to determine color of text area: gray, red or white)
+// send to local storage
+localStorage.setItem("userInput", JSON.stringify(userInput));
+}
 
-//- assign proper class (.past .present .future) based on comparison to current hour (<,>, === or <=, >=, ===)
+// click event attached to save button class to run function
+$(".saveBtn").click(function() {
+    writeToLocalStorage();
+})
 
-        }
-    }
-
-
-//     function writeToLocalStorage ()
-//     <!-- this is to save a value from the text area to local storage, using the hour as the key when save button is clicked -->
-//         add click event to save button class to run function
-//             - read text from text area (class = "description", should be parent of save button)
-//             - read schedule hour
-//             - trim text, trim schedule hour
-//             - send to local storage
-
-
-
-//     call readFromLocalStorage(); 
+// call readFromLocalStorage(); 
 readFromLocalStorage(); 
